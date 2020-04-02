5440e833bb0e72ff1a406e93e4a08284962464db
Gokoura <dumontdd59@gmail.com>
2020-04-02 10:44:57 +0200

    barba MAJ

--- public/js/main.js
+++ public/js/main.js
@@ -10,18 +10,10 @@ website: karanmhatre.com
 function pageTransition() {
 
     var tl = gsap.timeline();
-    tl.to('.logo_loader', { duration: .01, opacity:"1"});
     tl.set('.loading-screen', { transformOrigin: "bottom right"});
-    tl.to('.loading-screen', { duration: .5, scaleY: 1});
-    tl.to('.logo_loader', { duration: .5, left: "45vw"});
-    tl.to('.logo_loader', { duration: .2,});
-    tl.to('.logo_loader', { duration: .2, scale:"1.5"});
-    tl.to('.logo_loader', { duration: .2, scale:"1"});
-    tl.to('.logo_loader', { duration: .2,});
-    tl.to('.logo_loader', { duration: .5, left:"110vw"});
-    tl.to('.loading-screen', { duration: .5, scaleY: 0, skewX: 0, transformOrigin: "top right", ease: "power1.out"});
-    tl.to('.logo_loader', { duration: .01, opacity:"0"});
-    tl.to('.logo_loader', { duration: .01, left:"-20vw"});
+    tl.to('.loading-screen', { duration: .4, scaleY: 1});
+    tl.to('.loading-screen', { duration: .15});
+    tl.to('.loading-screen', { duration: .4, scaleY: 0, skewX: 0, transformOrigin: "top right", ease: "power1.out"});
 }
 
 // Function to animate the content of each page
@@ -39,7 +31,7 @@ $(function() {
             async leave(data) {
                 const done = this.async();
                 pageTransition();
-                await delay(1000);
+                await delay(500);
                 done();
             },
             async enter(data) {
